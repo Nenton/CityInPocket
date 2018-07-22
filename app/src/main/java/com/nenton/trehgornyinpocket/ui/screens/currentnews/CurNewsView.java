985fc80b2fc94d29b7067503551726ae50d56d39
= <sergejsusev@yandex.ru>
2018-07-22 19:26:27 +0500

    feature: Add pager adapter and bind cur news view

--- app/src/main/java/com/nenton/trehgornyinpocket/ui/screens/currentnews/CurNewsView.java
+++ app/src/main/java/com/nenton/trehgornyinpocket/ui/screens/currentnews/CurNewsView.java
@@ -1,12 +1,29 @@
 package com.nenton.trehgornyinpocket.ui.screens.currentnews;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
+import com.nenton.trehgornyinpocket.R;
+import com.nenton.trehgornyinpocket.data.storage.dto.NewsDto;
 import com.nenton.trehgornyinpocket.di.DaggerService;
 import com.nenton.trehgornyinpocket.mvp.views.AbstractView;
 
+import butterknife.BindView;
+
 public class CurNewsView extends AbstractView<CurNewsScreen.CurNewsPresenter> {
+    @BindView(R.id.cur_news_title)
+    TextView titleView;
+    @BindView(R.id.cur_news_description)
+    TextView descriptionView;
+    @BindView(R.id.cur_news_photos)
+    ViewPager photosViews;
+    @BindView(R.id.cur_news_date)
+    TextView date;
+
+    private CurNewsAdapter adapter;
+
     public CurNewsView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -19,5 +36,20 @@ public class CurNewsView extends AbstractView<CurNewsScreen.CurNewsPresenter> {
     @Override
     public boolean viewOnBackPressed() {
         return false;
+    }
+
+    public void initView(NewsDto currentNews) {
+        titleView.setText(currentNews.getDescription());
+        descriptionView.setText(currentNews.getFullNew());
+        date.setText(currentNews.getDate().toString());
+
+        adapter = new CurNewsAdapter();
+        photosViews.setAdapter(adapter);
+
+        for (String urlPhotos : currentNews.getImagesUrl()) {
+            adapter.addUrl(urlPhotos);
+        }
+        adapter.addUrl("https://avatars.mds.yandex.net/get-pdb/812271/1934c8a2-a8f3-4b18-8ed5-7683e9842bfb/s1200");
+        adapter.addUrl("https://avatars.mds.yandex.net/get-pdb/70729/6b068f73-2c77-4d10-927e-9fd5b2ee2302/s1200");
     }
 }
