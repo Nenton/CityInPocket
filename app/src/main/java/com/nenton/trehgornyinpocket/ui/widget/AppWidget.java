7d323a075f9c9452554b6861a644a0bbe769eca7
= <sergejsusev@yandex.ru>
2018-08-01 22:46:16 +0500

    feature: add test widget

--- app/src/main/java/com/nenton/trehgornyinpocket/ui/widget/AppWidget.java
+++ app/src/main/java/com/nenton/trehgornyinpocket/ui/widget/AppWidget.java
@@ -7,6 +7,8 @@ import android.widget.RemoteViews;
 
 import com.nenton.trehgornyinpocket.R;
 
+import java.util.Set;
+
 /**
  * Implementation of App Widget functionality.
  * App Widget Configuration implemented in {@link AppWidgetConfigureActivity AppWidgetConfigureActivity}
@@ -16,14 +18,37 @@ public class AppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = AppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        Set<String> widgetText = AppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, "БАЛДА");
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+//        setUpdateRv(views, context, appWidgetId);
+//        setList(views, context, appWidgetId);
+//
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
     }
+
+//    private static void setList(RemoteViews views, Context context, int appWidgetId) {
+//        Log.e("Widget", "setList");
+//        Intent intent = new Intent(context, MyService.class);
+//        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+//        views.setRemoteAdapter(R.id.widget_list, intent);
+//    }
+//
+//    private static void setUpdateRv(RemoteViews views, Context context, int appWidgetId) {
+//        Log.e("Widget", "setUpdateRv");
+//        Intent intent = new Intent(context, Widget.class);
+//        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new int[]{appWidgetId});
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, 0);
+//        views.setOnClickPendingIntent(R.id.widget_tv, pendingIntent);
+//    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
